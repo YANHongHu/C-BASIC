165d704722be197c8baf5a2cb4a813d3f7be49ed
YANHongHu <49400532+YANHongHu@users.noreply.github.com>
2019-04-23 17:00:45 +0800

    不用临时变量，交换两个整数值

--- C-BASIC/project1.c
+++ C-BASIC/project1.c
@@ -2,13 +2,12 @@
 #include<stdlib.h>
 
 main() {
-		int num1=1;
+		int num1=4;
 		int num2=3;
 		printf("%d,%d\n", num1, num2);
-		int num3;
-		num3 = num2;
-		num2 = num1;
-		num1 = num3;
+		num1 = num1 + num2;
+		num2 = num1 - num2;
+		num1 = num1 - num2;
 		printf("%d,%d\n", num1, num2);
 		system("pause");
 		return 0;
