da0e79f5fcc11cba2b2d50be142c77e9bd81e340
YANHongHu <49400532+YANHongHu@users.noreply.github.com>
2019-04-23 17:28:27 +0800

    Update project1.c

--- C-BASIC/project1.c
+++ C-BASIC/project1.c
@@ -2,16 +2,30 @@
 #include<stdlib.h>
 
 main() {
-	int a[10] = { 10,3,4,5,6,2,1,7,8,9 };
-	int max;
-	int i;
-	max = a[0];
-	for (i = 1; i <= 9; i++) {
-		if (a[i] > max) {
-			max = a[i];
+	int a[10] = { 10,3,4 };
+		if (a[0] > a[1]) {
+			if (a[0] > a[2]) {
+				printf("%d ", a[0]);
+				if (a[1] > a[2]) {
+					printf("%d %d", a[1], a[2]);
+				}
+				else
+					printf("%d %d", a[2], a[1]);
+			}
+			else printf("%d %d %d", a[2],a[0],a[1]);
 		}
-	}
-	printf("%d\n", max);
+		else {
+			if (a[1] > a[2]) {
+				printf("%d ", a[1]);
+				if (a[2] > a[0]) {
+					printf("%d %d", a[2], a[0]);
+				}
+				else
+					printf("%d %d", a[0], a[2]);
+			}
+			else
+				printf("%d %d %d", a[2], a[1, a[0]]);
+		}
 	system("pause");
 	return 0;
 }
